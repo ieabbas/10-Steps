d07e45593fe584f8556a7e7fd9e00b99aade8364
Ismail A <ieabbas@users.noreply.github.com>
2016-12-13 20:51:48 -0800

    Create UI.java

---
+++ UI.java
@@ -0,0 +1,22 @@
+package 10Steps;
+
+/*
+ * A variant of an academic project put forth by Professor Edwin Rodriguez of Cal Poly Pomona, this is a 
+ * console baseed RPG where you control(insert your hero/heroine's name here) and attempt to escape a 
+ * trap they found themselves inside a cave. Armed with one of three weapons chosen by the player at the start of 
+ * the game, the hero/heroine is 10 steps away from the cave, and freedom. Every step they take however, there is 
+ * a chance to encounter a thug(unarmed or armed with a randomly chosen weapon). You may try to fight, take a step
+ * back, or attempt to escape (Pokemon style). Don't forget your power ups either, an extra magazine 
+ * and a health pack may come in handy! No matter what, one thing is for certain however, it'll be the most 
+ * difficult 10 steps you ever take.
+ * 
+ * 
+ * Creator: ieabbas
+ */
+
+/*
+ * This class will hold any message that is outputted by the game to the user.
+ */
+
+public class UI {
+}
