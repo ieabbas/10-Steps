ca79cb307c6785ca58a63d59d37bd33f355f770c
Ismail A <ieabbas@users.noreply.github.com>
2017-06-17 02:25:43 -0700

    Update GameEngine.java

--- consoleVersion/GameEngine.java
+++ consoleVersion/GameEngine.java
@@ -1,5 +1,7 @@
 package consoleVersion;
 
+import java.util.*;
+
 /*
  * A variant of an academic project put forth by Professor Edwin Rodriguez of Cal Poly Pomona, this is a 
  * console baseed RPG where you control(insert your hero/heroine's name here) and attempt to escape a 
@@ -15,14 +17,30 @@ package consoleVersion;
  */
 public class GameEngine {
 
+	private static Scanner kb = new Scanner(System.in);
+	private static Player heroGuy;
+	private static UI engine = new UI();
+
 	/*
 	 * The main method, only here to start the game
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		startGame();
 	}
 
-	public static void startGame() {
+	public static void startGame() throws Exception {
+		// Testing the player instantion
+		engine.welcomeMessage();
+		engine.setTheScene();
+		// Give player time to read
+		Thread.sleep(10000);
+		System.out.println("\nProgram waited, time to leave!");
+		System.exit(0);
+
+		// Rough exit predicted after the game finds it's end
+		// while (true) {
+		// System.out.println();
+		// }
 
 	}
 }
